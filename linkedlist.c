cc086cef4ee8829c0881584938c0947ce48eda34
TushitPalamkar <143091687+TushitPalamkar@users.noreply.github.com>
2023-09-10 17:48:26 +0530

    Add files via upload

---
+++ linkedlist.c
@@ -0,0 +1,142 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    struct node * next;
+    int data;
+};
+struct sll
+{
+    struct node * head;
+};
+
+char isempty(struct sll * l)
+{
+    if((*l).head==NULL)
+    {
+        printf("Empty list\n");
+    }
+}
+void traverse(struct sll * a)
+{
+    struct node * n;
+    n= (*a).head;
+    while(n!=NULL)
+    {
+        printf("%2d",n->data);
+        n=n->next;
+    }
+
+}
+void addfront(struct sll * b, struct node * s)
+{
+    
+    (*s).next=(*b).head;
+    (*b).head=s;
+}
+void addend(struct sll * c , struct node * m)
+{
+    struct node * n;
+
+    
+    n=(*c).head;
+    while((*n).next!=NULL)
+    {
+        n=(*n).next;
+    }
+    (*n).next=m;
+}
+void deletefront(struct sll * p)
+{
+    struct node * x;
+    x=(*p).head;
+    (*p).head=(*(*p).head).next;
+    free(x);
+}
+void deleteend(struct sll * k)
+{
+    struct node * n;
+    struct node * m;
+   (*m).next=NULL;
+    n= (*k).head;
+    while(n->next!=m)
+    {
+        n=(*n).next;
+    }
+    (*n).next=NULL;
+    free(m);
+}
+void main()
+{
+    int choice;
+    struct node * n;
+    struct sll * s;
+    struct sll S;
+    s=&S;
+    (*s).head=NULL;
+    n=NULL;
+    while(1)
+    {
+    printf("****************MENU****************\n");
+    do{
+    printf("1.isempty\n2.Traverse\n3.addfront\n4.addend\n5.deletefront\n6.deleteend\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1: 
+        
+        isempty(s);
+        break;
+
+        case 2: 
+        if(isempty)
+        printf("Can't be traversed");
+        else
+        {
+        traverse(s);
+        printf("\n");
+        }
+        break;
+
+        case 3: 
+        
+        struct node * n =(struct node *)malloc(sizeof(struct node));
+        printf("Enter the data to be added: ");
+        scanf("%d",&n->data);
+        (*n).next=NULL;
+        if((*s).head==NULL)
+        printf("List is empty, adding in the front of the list");
+        else
+        addfront(s,n);
+        break;
+
+        case 4: 
+    
+        struct node * m =(struct node *)malloc(sizeof(struct node));
+        printf("Enter the data to be added:");
+        scanf("%d",&((*m).data));
+        (*m).next=NULL;
+        if((*s).head==NULL)
+        printf("List is empty, adding at the end");
+        else
+        addend(s,m);
+        break;
+
+        case 5: 
+        if((*s).head==NULL)
+        printf("List is empty, cannot delete from the front of the list");
+        else
+        deletefront(s);
+        break;
+
+        case 6: 
+        if((*s).head==NULL)
+        printf("List is empty, cannot delete from the end of the list");
+        else
+        deleteend(s);
+        break;
+
+    }
+    }while(1);
+}
+}
---
+++ stack.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct student
+{
+    int rollno,marks;
+};
+struct stack 
+{
+    unsigned int size;
+    unsigned int top;
+    struct student * sarr;
+};
+
+char isempty(struct stack * s)
+{
+    if((*s).top==-1)
+    return 1;
+    else 
+    return 0;
+}
+char isfull(struct stack * s)
+{
+    if((*s).top==((*s).size-1))
+    return 1;
+    else
+    return 0;
+}
+void push(struct stack * s , struct student * x)
+{
+    if((*s).top==(*s).size-1)
+    {
+        printf("Stack overflow!");
+    }
+    else{
+        (*s).top++;
+        (*s).sarr[(*s).top]=*x;
+    }
+}
+void pop(struct stack * s)
+{
+    struct student temp;
+    if((*s).top==-1)
+    printf("Stack underflow!");
+    else{
+    temp=(*s).sarr[(*s).top];
+    (*s).top--;
+    }
+}
+void peek(struct stack * s)
+{
+    if(isempty(s))
+    printf("Stack underflow!");
+    else
+    {
+        struct student temp;
+        temp=(*s).sarr[(*s).top];
+        printf("\n");
+        printf("Roll no :%d\n",temp.rollno);
+        printf("Marks : %d",temp.marks);
+    }
+    
+    
+}
+int main()
+{
+    int choice,x;
+    struct stack * st = (struct stack *)malloc(sizeof(struct stack));
+    printf("Enter the size of the stack: ");
+    scanf("%d",&((*st).size));
+    (*st).top=0;
+    struct student * sarr=(struct student *)malloc(sizeof(struct student));
+    st->sarr=(struct student *)malloc((st->size+1)* sizeof(struct student));
+    do{
+    printf("***********MENU***********\n");
+    printf("1.IsEmpty\n2.IsFull\n3.Push\n4.Pop\n5.Peek\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+        if(isempty(st)==1)
+        printf("Stack is empty\n");
+        break;
+        case 2:
+        if(isfull(st)==1)
+        printf("Stack is full\n");
+        break;
+        case 3:
+        printf("Enter the rollno to be pushed: ");
+        scanf("%d",&((*sarr).rollno));
+        printf("Enter the marks to be pusehd: ");
+        scanf("%d",&((*sarr).marks));
+    
+        push(st,sarr);
+        break;
+        case 4:
+        pop(st);
+        break;
+        case 5:
+        peek(st);
+        printf("\n");
+        break;
+    }
+    }while(1);
+    
+}
